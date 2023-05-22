e90df25401c60e31aa383469818ea5dff49a1444
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-05-22 14:41:21 +0900

    feat: 영화 상세화면 구현완료

---
+++ src/screens/MovieDetailScreen/MovieDetailScreen.style.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const movieDetailScreenStyles = StyleSheet.create({
+  container: {
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  image: {
+    width: 300,
+    height: 300
+  }
+});
---
+++ src/screens/MovieDetailScreen/MovieDetailScreen.tsx
@@ -0,0 +1,22 @@
+import { RouteProp, useRoute } from '@react-navigation/native';
+import { Image, Text, View } from 'react-native';
+import { Movie } from 'types/movie';
+import { movieDetailScreenStyles } from './MovieDetailScreen.style';
+
+type ParamList = {
+  movieType: {
+    movie: Movie
+  };
+};
+
+const MovieDetailScreen = () => {
+  const { params } = useRoute<RouteProp<ParamList, 'movieType'>>();
+  return (
+    <View style={movieDetailScreenStyles.container}>
+      <Image style={movieDetailScreenStyles.image} source={{ uri: params.movie.medium_cover_image }}/>
+      <Text>{ params.movie.description_full }</Text>
+    </View>
+  );
+};
+
+export default MovieDetailScreen;
