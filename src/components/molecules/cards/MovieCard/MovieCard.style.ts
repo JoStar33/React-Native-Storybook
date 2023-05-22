5a4f9ec22595e94c9dfdfa89ecf8754a2596605b
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-05-22 17:42:57 +0900

    refact: 영화카드가 웹환경에서도 그림자가 적용되도록 수정

--- src/components/molecules/cards/MovieCard/MovieCard.style.ts
+++ src/components/molecules/cards/MovieCard/MovieCard.style.ts
@@ -25,6 +25,15 @@ export const movieCardStyles = StyleSheet.create({
       android: {
         elevation: 20,
       },
+      web: {
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 10,
+          height: 10,
+        },
+        shadowOpacity: 0.5,
+        shadowRadius: 10,
+      }
     }),
   },
   image: {
