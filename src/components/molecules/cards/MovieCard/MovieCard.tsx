765d7eeff67cf86de0b46f09c700750fb04719fd
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-05-22 14:43:01 +0900

    feat: 영화 카드 라우팅 기능 구현

--- src/components/molecules/cards/MovieCard/MovieCard.tsx
+++ src/components/molecules/cards/MovieCard/MovieCard.tsx
@@ -1,15 +1,26 @@
-import { Image, Text, TouchableOpacity } from 'react-native';
-import { Movie } from 'types/movie';
-import { movieCardStyles } from './MovieCard.style';
+import { useNavigation } from "@react-navigation/native";
+import { Menus } from "constants/menus";
+import { Image, Text, TouchableOpacity } from "react-native";
+import { Movie } from "types/movie";
+import { movieCardStyles } from "./MovieCard.style";
 
 interface Props {
   item: Movie;
 }
 
-const MovieCard = ({item}: Props) => {
+const MovieCard = ({ item }: Props) => {
+  const navigation = useNavigation();
+  const handleClick = () => {
+    navigation.navigate(Menus.MovieDetailScreen as never, {
+      movie: item
+    } as never);
+  };
   return (
-    <TouchableOpacity style={movieCardStyles.container}>
-      <Image style={movieCardStyles.image} source={{uri: item.medium_cover_image}}/>
+    <TouchableOpacity style={movieCardStyles.container} onPress={handleClick}>
+      <Image
+        style={movieCardStyles.image}
+        source={{ uri: item.medium_cover_image }}
+      />
       <Text style={movieCardStyles.title}>{item.title}</Text>
       <Text style={movieCardStyles.description}>{item.description_full}</Text>
     </TouchableOpacity>
