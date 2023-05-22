80715fbf5533f390d8ef6e953ee588a90a2350b2
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-05-22 16:51:47 +0900

    feat: 토글 스위치 구현

---
+++ src/components/atoms/switchs/CommonSwitch.tsx
@@ -0,0 +1,18 @@
+import { Switch } from "react-native";
+
+interface Props {
+  isEnabled: boolean;
+  toggleSwitch: () => void;
+}
+
+const CommonSwitch = ({isEnabled, toggleSwitch}: Props) => {
+  return <Switch
+    trackColor={{false: '#767577', true: '#81b0ff'}}
+    thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+    ios_backgroundColor="#3e3e3e"
+    onValueChange={toggleSwitch}
+    value={isEnabled}
+  />;
+};
+
+export default CommonSwitch;
