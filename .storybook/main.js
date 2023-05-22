acf7235089a0274670e8c4f97a814a6e2e64bebc
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-05-22 16:52:47 +0900

    fix: 스토리북 상에서 이모티콘이 깨지던 이슈해결

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,11 @@
+const path = require('path');
+
 module.exports = {
   stories: ["../src/**/*.stories.?(ts|tsx|js|jsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-react-native-web",
+    "@storybook/addon-react-native-web"
   ],
   framework: "@storybook/react",
   core: {
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,25 @@
+import { useEffect } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 const queryClient = new QueryClient();
 
-export const decorators = [(Story) => (
-  <QueryClientProvider client={queryClient}>
-    <Story />
-  </QueryClientProvider>
-)];
+export const decorators = [(Story) => {
+  useEffect(() => {
+    const iconFontStyles = `@font-face {
+      src: url(${require('react-native-vector-icons/Fonts/FontAwesome.ttf')}) format(truetype);
+      font-family: "FontAwesome";
+    }`;
+    // Create stylesheet
+    const style = document.createElement('style');
+    style.appendChild(document.createTextNode(iconFontStyles));
+    // Inject stylesheet
+    document.head.appendChild(style);
+  }, []);
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Story />
+    </QueryClientProvider>
+  )
+}];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
