6a91a6dba882233f3592c8daf22ecfb60fec4f99
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-05-23 17:57:27 +0900

    chore: 코드에 주석으로 설명 추가

--- src/Navigator.tsx
+++ src/Navigator.tsx
@@ -6,7 +6,9 @@ const Stack = createStackNavigator();
 const MainScreen = () => {
   return (
     <Stack.Navigator initialRouteName="Home">
+      {/* 상단 네비게이터 */}
       <Stack.Screen name="TopTabNavigator" component={TopTabNavigator} />
+      {/* 일반 네비게이터 */}
       <Stack.Screen name="MovieDetailScreen" component={MovieDetailScreen} />
     </Stack.Navigator>
   );
