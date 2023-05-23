b87c9e948d9b62a8e549ae1d4c3ec85bbc6acac5
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-05-23 15:05:59 +0900

    feat: 겹치는 스타일 믹싱화

---
+++ src/styles/mixinStyle.ts
@@ -0,0 +1,40 @@
+import { Platform, StyleSheet } from "react-native";
+import { vh, vw } from "rxn-units";
+
+const mixinStyle = StyleSheet.create({
+  rowCardContainer: {
+    marginTop: vh(2),
+    width: vw(98),
+    height: vh(30),
+    borderRadius: 20,
+    padding: vh(2),
+    overflow: "hidden",
+    backgroundColor: "white",
+    ...Platform.select({
+      ios: {
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 10,
+          height: 10,
+        },
+        shadowOpacity: 0.5,
+        shadowRadius: 10,
+      },
+      android: {
+        elevation: 20,
+      },
+      web: {
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 10,
+          height: 10,
+        },
+        shadowOpacity: 0.5,
+        shadowRadius: 10,
+      },
+    }),
+  }
+});
+
+export { mixinStyle };
+
